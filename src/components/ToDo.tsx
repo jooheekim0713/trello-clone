6a8cf58367831f7804e6a976a49a2609e4d8ab9b
jooheekim0713 <jooheekim.dev@gmail.com>
2022-07-29 17:33:44 +0900

    Segregate todo into todo, doing,done list

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,9 +1,45 @@
-import { IToDo } from '../atom';
+import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { IToDo, toDoState } from '../atom';
 
-function ToDo(toDo: IToDo) {
+function ToDo({ text, id, category }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name as any };
+
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
-    <li key={toDo.id}>
-      <span>{toDo.text}</span>
+    <li key={id}>
+      <span>{text}</span>
+      {category !== 'TO_DO' && (
+        <button name="TO_DO" onClick={onClick}>
+          TO_DO
+        </button>
+      )}
+
+      {category !== 'DOING' && (
+        <button name="DOING" onClick={onClick}>
+          DOING
+        </button>
+      )}
+
+      {category !== 'DONE' && (
+        <button name="DONE" onClick={onClick}>
+          DONE
+        </button>
+      )}
     </li>
   );
 }
